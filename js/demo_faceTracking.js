4f3de5361af41d4ed51eb38432358ec5f65b5cab
yehwansong <yehwanyensong@gmail.com>
2020-01-22 17:55:43 +0900

    add

--- js/demo_faceTracking.js
+++ js/demo_faceTracking.js
@@ -3,6 +3,9 @@
     var h = 1000
 
 $('document').ready(function(){
+    console.log((w - window.innerWidth)/2 - 75 + 'px')
+    $('#face_wrap').css({'margin-left' : (window.innerWidth - w)/2 - 75 + 'px'})
+    $('#face_wrap').css({'margin-top' : (window.innerHeight - h)/2 - 50 +'px'})
 var canv = document.createElement('canvas');
 canv.id = 'jeeFaceFilterCanvas';
 canv.width = w
@@ -14,6 +17,10 @@ main()
 function main(){
     var CVD; //return of Canvas2DDisplay
 
+        var or_x = 0;
+        var or_y = 0;
+        var or_z = 0;
+        var counter = 0
 const SETTINGS={
   maxFaces: 4, //max number of detected faces
 };
@@ -29,32 +36,57 @@ const SETTINGS={
 
             console.log('INFO : JEEFACEFILTERAPI IS READY');
             CVD = JEEFACEFILTERAPI.Canvas2DDisplay(spec);
-            console.log(CVD)
             CVD.ctx.strokeStyle='#ffffffff';
             CVD.ctx.lineWidth=0;
         }, //end callbackReady()
 
         //called at each render iteration (drawing loop)
-
         callbackTrack: function(detectState){
             CVD.ctx.clearRect(0,0,CVD.canvas.width, CVD.canvas.height);
             for (var i = detectState.length - 1; i >= 0; i--) {
+
+                if(Math.abs(detectState[0].rx - or_x) > 0.1){
+                    or_x = detectState[0].rx
+                    counter++
+                    $('#face_wrap>div').hide()
+                    $('#face_' + (counter%4+1)).show()
+                }
+
+                if(Math.abs(detectState[0].ry - or_y) > 0.1){
+                    or_y = detectState[0].ry
+                    counter++
+                    $('#face_wrap>div').hide()
+                    $('#face_' + (counter%5+5)).show()
+                }
+                
+                // or_y = detectState[i].ry
+                // or_z = detectState[i].rz
                     if (detectState[i].detected>0.3){
-                    //draw a border around the face
                     var faceCoo=CVD.getCoordinates(detectState[i]);
-                    // -1~1
-                    Body.setPosition(face1, { x: map_range(detectState[i].x, 1, -1, 0, w), y:map_range(detectState[i].y, 1, -1, 0, h)}); 
-                    setTimeout(function(){
-                        // engine.world.gravity.y = 0.5;
-                    }, 1000);
+                    $('#face_wrap').css({top:map_range(detectState[i].y, 1, -1, 0, h)})
+                    $('#face_wrap').css({'transform':'rotateX('+ map_range(detectState[i].rx, 1, -1, 90, -90)  +'deg) \
+                                                rotateY('+ map_range(detectState[i].ry, 1, -1, 90, -90)  +'deg) \
+                                                rotateZ('+ map_range(detectState[i].rz, 1, -1, 90, -90)  +'deg) translateZ(-50px)'})
+                    $('#face_wrap').css({left:map_range(detectState[i].x, 1, -1, 0, w)})
+                    Body.setPosition(face1, { x: map_range(detectState[i].x, 1, -1, 0, w) , y:map_range(detectState[i].y, 1, -1, 0, h)}); 
+                    if(faceCoo.x<200 && faceCoo.y<500){
+                        $('#background').css({opacity:0})
+                        $('#background_back').css({opacity:0})
+                    }else if(faceCoo.x>500 && faceCoo.y<500){
+                        $('#background').css({opacity:0})
+                        $('#background_back').css({opacity:1})
+                    }else{
+                        $('#background').css({opacity:1})
+                        $('#background_back').css({opacity:1})
+                    }
 
                     CVD.ctx.strokeRect(faceCoo.x, faceCoo.y, faceCoo.w, faceCoo.h);
                 }
             }
             CVD.update_canvasTexture();
             CVD.draw();
-        } //end callbackTrack()
-    }); //end JEEFACEFILTERAPI.init call
+        } 
+    }); 
 } //end main()
 function map_range(value, low1, high1, low2, high2) {
     return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
